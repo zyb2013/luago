71259a6a4140e6b8d2b6a29a62432522c3dc90ae
zhuyuanbiao <zhuyuanbiao@yaowan.com>
2018-11-21 09:26:17 +0800

    增加常量

--- src/binchunk/binary_chunk.go
+++ src/binchunk/binary_chunk.go
@@ -1,5 +1,19 @@
 package binchunk
 
+const (
+	LUA_SIGNATURE = "\x1bLua"
+	LUAC_VERSION = 0x53
+	LUAC_FORMAT = 0
+	LUAC_DATA = "\x19\x93\r\n\x1a\n"
+	CINT_SIZE = 4
+	CSZIET_SIZE = 8
+	INSTRUCTION_SIZE = 4
+	LUA_INTEGER_SIZE = 8
+	LUA_NUMBER_SIZE = 8
+	LUAC_INT = 0x5678
+	LUAC_NUM = 370.5
+)
+
 type binaryChunk struct {
 	header // 头部
 	upvaluesSize byte // 主函数upvalue数量
